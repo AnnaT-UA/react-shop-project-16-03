5b1890ab4c00eedef5e0ee31969e1142115c5294
Hanna <trehubhanna@gmail.com>
2024-06-30 13:12:59 -0400

    added files with function declaration

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//react Element
-// const h1 = React.createElement('h1', { id: 'title' }, 'Hello app')
-// const p = React.createElement('p', null, 'Blablablabla')
+///function declaration
 
-// const app = (
-//     <div>
-//         {h1}
-//         {p}
-//     </div>
-// )
+function Title() {
+    return <h1>Hello Title Component</h1>
+}
 
-const h1 = <h1 id="title"> Hello App</h1>
-const p = (
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam iure
-        cumque consectetur tenetur omnis laudantium quos sint sunt neque earum
-        aut iusto rerum, officia distinctio saepe reiciendis, commodi delectus
-        voluptas.
-    </p>
-)
-
-const ul = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 2</li>
-    </ul>
-)
-
-const app = (
-    <div>
-        {h1}
-        {p}
-        {ul}
-    </div>
-)
+function Content() {
+    return (
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam iure
+            cumque consectetur tenetur omnis laudantium quos sint sunt neque
+            earum aut iusto rerum, officia distinctio saepe reiciendis, commodi
+            delectus voluptas.
+        </p>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <Title />
+        <Content />
+    </React.StrictMode>
+)
