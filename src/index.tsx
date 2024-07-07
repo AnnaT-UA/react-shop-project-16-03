fe858b36b487c8507ad451d55bb9983094399d5e
Hanna <trehubhanna@gmail.com>
2024-07-07 12:50:24 -0400

    added content into separate folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header'
-
-type ContentType = {
-    title: string
-    text1: string
-    text2: string
-}
-
-const Content = ({ title, text1, text2 }: ContentType) => {
-    return (
-        <>
-            <h2>{title}</h2>
-            <p>{text1}</p>
-            <p>{text2}</p>
-        </>
-    )
-}
+import Content from './Content'
 
 const App = () => {
     return (
