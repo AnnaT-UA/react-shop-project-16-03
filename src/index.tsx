472e010d1494e378c1059531e0fb46daa9eafa83
Hanna <trehubhanna@gmail.com>
2024-07-13 12:52:38 -0400

    створии mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header'
-import Content from './Content'
-
-const App = () => {
-    return (
-        <>
-            <Header
-                order={3}
-                text="Title"
-                leftPart="Hello"
-                rightPart="Test"
-                background="aqua"
-                color="green"
-            />
-            <Header
-                text="App"
-                leftPart="Hi"
-                rightPart="Bla-Bla"
-                background="green"
-                color="yellow"
-            />
-            <Header text="React" leftPart="Bye-bye" rightPart="Rainbow" />
-            <Content title="Content 1" text1="hello 1" text2="Hello 2" />
-            <Content
-                title="Content 2"
-                text1="hello 1"
-                text2="Hello 2 lorem blalb alblablbl"
-            />
-        </>
-    )
-}
+import App from './layout/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
     <React.StrictMode>
         <App />
