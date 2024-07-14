ca318a216d03e415b49acf007cd12b6b3aacc9fd
Hanna <trehubhanna@gmail.com>
2024-07-14 13:02:44 -0400

    added absolute import Menu,Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import MenuIcon from '@mui/icons-material/Menu'
-import logo from '../../assets/logo.svg'
+import logo from 'assets/logo.svg'
 
 type Props = {}
 const Logo = (props: Props) => {
