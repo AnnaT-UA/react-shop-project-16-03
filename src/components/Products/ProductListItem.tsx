da13a243d3134e8b4d2226e1c5ade970100e6df3
Hanna <trehubhanna@gmail.com>
2024-07-21 11:20:01 -0400

    added description with props

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -3,19 +3,17 @@ import { Button, Card, CardContent } from '@mui/material'
 type Props = {
     title: string
     description: string
-    capacity: string
-    price: string
 }
 
-const ProductListItem = ({ title } { description } { capacity } { price }: Props) => {
+const ProductListItem = ({ title, description }: Props) => {
     return (
         <Card variant="outlined" className="product-list-item">
             <CardContent>
                 <h3 className="product-title">{title}</h3>
                 <p className="product-description">{description}</p>
                 <div className="product-features">Type: phone</div>
-                <div className="product-features">{capacity}</div>
-                <div className="product-price">{price}</div>
+                <div className="product-features">Capacity: 64Gb</div>
+                <div className="product-price">$ 800</div>
                 <div className="btns-wrapper">
                     <Button variant="outlined">Add to cart</Button>
                 </div>
